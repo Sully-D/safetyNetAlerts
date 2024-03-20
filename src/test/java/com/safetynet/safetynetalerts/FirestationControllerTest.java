4867fc9ae3c44b23dbc353c1fcd99c9da80862f3
sully <delemos.sullivan@gmail.com>
2024-03-20 07:17:32 +0100

    Test

--- src/test/java/com/safetynet/safetynetalerts/FirestationControllerTest.java
+++ src/test/java/com/safetynet/safetynetalerts/FirestationControllerTest.java
@@ -1,25 +1,5 @@
 package com.safetynet.safetynetalerts;
 
-//import com.safetynet.safetynetalerts.controller.FirestationController;
-//import com.safetynet.safetynetalerts.model.Firestation;
-//import com.safetynet.safetynetalerts.repository.ImplEncapsulateModelsPrsFstMdrDAOFirestation;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.hamcrest.Matchers.containsString;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.doAnswer;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.safetynet.safetynetalerts.controller.FirestationController;
 import com.safetynet.safetynetalerts.model.Firestation;
 import com.safetynet.safetynetalerts.repository.ImplEncapsulateModelsPrsFstMdrDAOFirestation;
